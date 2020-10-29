5571b648d0a60d494606d2f68fde19a82940fae2
ramos07 <ramosricardo96@gmail.com>
2020-10-28 17:34:31 -0700

    Created registration route

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,6 +21,36 @@ const authUser = asyncHandler( async(req, res) => {
     }
 })
 
+const registerUser = asyncHandler( async(req, res) => {
+    const { name, username, password, isAdmin } = req.body
+
+    const userExists = await User.findOne({ username })
+
+    if(userExists){
+        res.status(400)
+        throw new Error('User already exists!')
+    }
+
+    const user = await User.create({
+        name,
+        username,
+        password,
+        isAdmin
+    })
+
+    if(user){
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            username: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+        })
+    }else{
+        res.status(400)
+        throw new Error("Invalid user data")
+    }
+})
 
 const getUserProfile = asyncHandler( async(req, res) => {
     const user = await User.findById(req.user._id)
@@ -38,4 +68,4 @@ const getUserProfile = asyncHandler( async(req, res) => {
     }
 })
 
-export { authUser, getUserProfile}
+export { authUser, getUserProfile, registerUser}
